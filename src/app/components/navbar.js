8071d8143ebf0885ae949251a5756bed6d947884
unknown <tech2@imperio.com>
2024-10-05 16:50:54 +0530

    Changes

--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -380,7 +380,7 @@ export default function Navbar() {
                 >
                   <Link
                     href={item.href}
-                    className="px-3 py-2 rounded-md text-xl font-medium hover:text-white flex items-center"
+                    className="px-3 py-2 rounded-md text-base hover:text-white flex items-center poppins-regular"
                   >
                     {item.name}
                     {item.name === "Products" && (
